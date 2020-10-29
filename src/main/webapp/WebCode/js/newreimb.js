dde1e8bc5c6b24b386c6650b400fb0c1388f621f
Paul <benderp94@gmail.com>
2020-10-28 18:15:22 -0700

    Redeploying war once again

--- src/main/webapp/WebCode/js/newreimb.js
+++ src/main/webapp/WebCode/js/newreimb.js
@@ -1,5 +1,27 @@
 
 
+
+function addReimbursement(){
+   const reimbursement = {
+      reimbursementId: 100,
+      reimbursementAmount: document.getElementById("reimb_amount").value,
+      submittedDate: document.getElementById("reimb_submitted").value,
+      resolvedDate: '2000-10-10',
+      description: document.getElementById("reimb_description").value,
+      reimbursementAuthor: 1,
+      reimbursementResolver: 1,
+      reimbursementStatusId: 1,
+      reimbursementTypeId: document.getElementById("reimb_type_id").value 
+   }
+  const fetched = await fetch('http://52.42.213.40:8082/Project1/reimb.json',{
+       body: reimbursement,
+       method: 'post',
+       mode: "no-cors",
+       headers:{
+           origin: 'localhost'
+       }
+  }) 
+}
 /*
 async function addReimbursement(){
     const reimbursement = {
